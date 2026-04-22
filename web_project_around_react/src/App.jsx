1aa96088ebe647f59770ec91a2cc05680bbcae9e
Rixuk <hugomh90@hotmail.com>
2026-04-22 17:24:40 -0600

    Primer push con cambios

--- web_project_around_react/src/App.jsx
+++ web_project_around_react/src/App.jsx
@@ -1,119 +1,194 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import vector from "../images/Vector.png"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta
+      name="description"
+      content="Página que nos permite ver lugares de Estados Unidos"
+    />
+    <meta name="keywords" content="locations, project" />
+    <meta name="author" content="Rixuk" />
+    <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="../pages/index.css" />
+    <title>Around the U.S.</title>
+  </head>
+  <body>
+    <div class="page">
+      <header class="header">
+        <h1 class="header__title">
+          Around <span id="header__sup"><sup>The U.S.</sup></span>
+        </h1>
+      </header>
+      <main class="content">
+        <section class="profile">
+          <div class="profile__picture">
+            <img src="" alt="avatar image" class="profile__avatar"/>
+            <div class="profile__edit-icon-container">
+              <img src={vector} alt="edit icon" class="profile__edit-icon"/>
+            </div>
+          </div>
+          <div class="profile__info">
+            <div class="profile__name-and-button">
+              <h2 class="profile__name" id="profile__name"></h2>
+              <button class="profile__button"></button>
+            </div>
+            <p class="profile__profession" id="profile__profession"></p>
+          </div>
+          <div class="profile__add">
+            <button class="profile__add-button">+</button>
+          </div>
+          <div class="popups" id="popup-profile">
+            <div class="popup__overlay"></div>
+            <div class="popup__window">
+              <h2 class="popup__title popup__style">Editar perfil</h2>
+              <form class="popup__form" novalidate>
+                <fieldset class="popup__fieldset">
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="Nombre"
+                    class="popup__name popup__style form__inputs"
+                    id="popup__name"
+                    required
+                    minlength="2"
+                    maxlength="40"
+                  />
+                  <span
+                    class="form__inputs-error_active"
+                    id="popup__name-error"
+                  ></span>
+                  <input
+                    type="text"
+                    name="about"
+                    placeholder="Acerca de mí"
+                    class="popup__aboutme popup__style form__inputs"
+                    id="popup__job"
+                    required
+                    minlength="2"
+                    maxlength="200"
+                  />
+                  <span
+                    class="form__inputs-error_active"
+                    id="popup__job-error"
+                  ></span>
+                  <button type="submit" class="popup__save popup__style">
+                    Guardar
+                  </button>
+                </fieldset>
+              </form>
+              <button class="popups__close"></button>
+            </div>
+          </div>
+          <div class="popups" id="popup-cards">
+            <div class="popup__overlay"></div>
+            <div class="popup__window">
+              <h2 class="popup__title popup__style">Nuevo lugar</h2>
+              <form class="popup__form" novalidate>
+                <fieldset class="popup__fieldset">
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="Título"
+                    class="popup__name popup__style form__inputs"
+                    id="new-cards__name"
+                    required
+                    minlength="2"
+                    maxlength="30"
+                  />
+                  <span
+                    class="form__inputs-error_active"
+                    id="new-cards__name-error"
+                  ></span>
+                  <input
+                    type="url"
+                    name="link"
+                    placeholder="Enlace a la imagen"
+                    class="popup__aboutme popup__style form__inputs"
+                    id="new-cards__link"
+                    required
+                  />
+                  <span
+                    class="form__inputs-error_active"
+                    id="new-cards__link-error"
+                  ></span>
+                  <button type="submit" class="popup__save popup__style">
+                    Crear
+                  </button>
+                </fieldset>
+              </form>
+              <button class="popups__close"></button>
+            </div>
+          </div>
+          <div class="popups" id="popup-confirmation">
+            <div class="popup__overlay"></div>
+            <div class="popup__window">
+              <h2 class="popup__title popup__style">¿Estás seguro?</h2>
+              <form class="popup__form" novalidate>
+                <fieldset class="popup__fieldset">
+                  <button type="submit" class="popup__save popup__style">
+                    Sí
+                  </button>
+                </fieldset>
+              </form>
+              <button class="popups__close"></button>
+            </div>
+          </div>
+          <div class="popups" id="popup-edit-profile">
+            <div class="popup__overlay"></div>
+            <div class="popup__window">
+              <h2 class="popup__title popup__style">Cambiar foto de perfil</h2>
+              <form class="popup__form" novalidate>
+                <fieldset class="popup__fieldset">
+                  <input type="text" name="avatar" placeholder="Enlace a la imagen" class="popup__avatar popup__style form__inputs" id="edit-avatar__link" required />
+                  <span
+                    class="form__inputs-error_active"
+                    id="edit-avatar__link-error"
+                  ></span>
+                  <button type="submit" class="popup__save popup__style">Guardar</button>
+                </fieldset>
+              </form>
+              <button class="popups__close"></button>
+            </div>
+          </div>
+        </section>
+        <section class="elements"></section>
+      </main>
+      <footer class="footer">
+        <p class="footer__copyright">&copy; 2024 Around The U.S. Original</p>
+      </footer>
+    </div>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
+    <template id="elements__template">
+      <div class="elements__card">
+        <img class="elements__image" src="" alt="" />
+        <img class="elements__trash" src="../images/Trash.png" alt="" />
+        <div class="elements__info">
+          <h2 class="elements__location"></h2>
+          <button class="elements__like"></button>
         </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+      </div>
+    </template>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+    <div class="popups" id="popup-image">
+      <div class="popup__overlay"></div>
+      <div id="popup-images__container">
+        <img id="popup-images__image" src="" alt="" />
+        <button class="popups__close"></button>
+        <p id="popup-images__location"></p>
+      </div>
+    </div>
+
+    <script src="../scripts/index.js" type="module"></script>
+  </body>
+</html>
     </>
   )
 }
