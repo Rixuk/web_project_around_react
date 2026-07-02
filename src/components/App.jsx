a7c1885aaab10a7b74ac5a5cd62e58b8dc97e874
Rixuk <hugomh90@hotmail.com>
2026-07-01 18:36:28 -0600

    fix files location

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
-import Header from "./components/Header/Header"
-import Main from "./components/Main/Main"
-import Footer from "./components/Footer/Footer"
+import Header from "./Header/Header"
+import Main from "./Main/Main"
+import Footer from "./Footer/Footer"
 import { useState, useEffect } from "react";
 
-import { api } from "./utils/api";
+import { api } from "../utils/api";
 
-import { CurrentUserContext } from "./contexts/CurrentUserContext";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
 function App() {
   /*----------------- Variables de estado ----------------*/
