8369ee80bb48bf77f8d22187de2ea553de9d4c79
Rixuk <hugomh90@hotmail.com>
2026-07-09 01:31:45 -0600

    add hide trahs icon

--- src/components/Main/components/Card/Card.jsx
+++ src/components/Main/components/Card/Card.jsx
@@ -1,10 +1,13 @@
 import thrashIcon from "../../../../../images/Trash.png";
+import { useContext } from "react";
+import { CurrentUserContext } from "../../../../contexts/CurrentUserContext";
 
 export default function Card(props){
     const {name, link, isLiked} = props.card;
     const {onCardClick} = props;
     const {onCardLike} = props;
     const {onDeletePopup} = props;
+    const { currentUser } = useContext(CurrentUserContext);
 
     const cardLikeButtonClassName = "elements__like" + (isLiked ? " elements__like-enabled" : "");
 
@@ -16,7 +19,11 @@ export default function Card(props){
                 alt={name}
                 onClick={() => onCardClick(props.card)}
             />
-            <img className="elements__trash" src={thrashIcon} alt="" onClick={() => onDeletePopup(props.card)} />
+
+            {/*ME PARECE QUE EN ESTE PUNTO DEL PROYECTO AÚN NO SE PUEDE CAMBIAR EL ID DEL USUARIO ACTUAL, PERO DEJO CONFIGURADA LA LÓGICA PARA CUANDO SE PUEDA*/}
+            { currentUser._id === props.card.owner && (
+                <img className="elements__trash" src={thrashIcon} alt="" onClick={() => onDeletePopup(props.card)} />
+            )}
             <div className="elements__info">
                 <h2 className="elements__location">{name}</h2>
                 <button className={cardLikeButtonClassName} aria-label="Like" type="button" onClick={() => onCardLike(props.card)}></button>
